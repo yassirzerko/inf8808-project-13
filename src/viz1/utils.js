c1c3058e1817942b481f4150baf10dcc4967e62b
Yassir El Aoufir <yassirelaoufir@gmail.com>
2022-06-22 02:37:28 -0400

    Tooltip working

--- src/viz1/utils.js
+++ src/viz1/utils.js
@@ -1,3 +1,29 @@
+export const CONSTANTS = {
+    variableSelector : {
+        values : ['Category','Genres', 'Type', 'Content Rating', 'Android Ver'],
+        texts : ['Categorie','Genres', 'Type', 'Evaluation de contenu', 'Version minimale d’Android requise'],
+        helper: 'Choisir la variable categorique a visualiser',
+        label: 'Variable'
+    },
+    downloadsMetricSelector : {
+        values : ['sum','avg', 'nApp', 'avgNApp'],
+        texts : ['Somme','Moyenne', 'Nombre applications avec +', 'Nombre applications avec + moyen'],
+        helper: 'Choisir la metrique de telechargement a visualiser',
+        label: 'Metrique de telechargements'
+    },
+    radioButtons: {
+        values : [true, false],
+        texts : ['Croissant','Decroissant'],
+        label:'Ordonnancement'
+    },
+    title : 'Visualisation 1 : Exploration du comportement des variables catégoriques et des téléchargements',
+    modal: {
+        title : 'Informations supplementaires',
+        content : 'TODO',
+    },
+  
+}
+
 export const getDownloadsRanges = (data) => {
     let uniques = new Set()
     for (let i = 0; i < data.length; i++) {
@@ -7,7 +33,7 @@ export const getDownloadsRanges = (data) => {
 }
 
 const handleSort = (preprocessedData, isAscending, downloadsMetric) => {
-    let sortMethod = isAscending === true ? (a, b) => b[downloadsMetric] - a[downloadsMetric] : (a, b) => a[downloadsMetric] - b[downloadsMetric]
+    let sortMethod = isAscending ? (a, b) => b[downloadsMetric].value - a[downloadsMetric].value : (a, b) => a[downloadsMetric].value - b[downloadsMetric].value
     preprocessedData.sort(sortMethod)
 }
 
@@ -29,55 +55,41 @@ const fillMaps = (data, variableName, sumDlsByValue, occurrencesByValue, nAppByV
     }
 }
 
+const addPositionsMetrics = (preprocessedData,) => {
+    const downloadsMetrics = CONSTANTS.downloadsMetricSelector.values
+    for (let i = 0 ; i < downloadsMetrics.length; i++) {
+        let downloadsMetric = downloadsMetrics[i]
+        handleSort(preprocessedData, false, downloadsMetric)
+
+        for (let j = 0; j < preprocessedData.length; j++) {
+            preprocessedData[j][downloadsMetric]['position'] = j
+        }
+    }
+}
+
+
 // Preprocess data for the first visualisation
 export const preprocessData = (data, downloadsMetric, variableName, isAscending, downloadsRange) => {
     let sumDlsByValue = new Map()
     let occurrencesByValue = new Map()
     let nAppByValue = new Map()
     
-    let shouldGetNApps = downloadsMetric === 'Nombre applications' || downloadsMetric === 'Nombre applications moyen'
-    fillMaps(data, variableName, sumDlsByValue, occurrencesByValue, shouldGetNApps ? [nAppByValue, downloadsRange] : null )
+    fillMaps(data, variableName, sumDlsByValue, occurrencesByValue,  [nAppByValue, downloadsRange] )
 
     let preprocessedData = []
     for (let [value, sumDls] of sumDlsByValue) {
         let distribution = occurrencesByValue.get(value) / data.length
         let meanDls = sumDls / occurrencesByValue.get(value)
-        let preprocessedValue = { 'value': value, 'brut': sumDls, 'moyen': meanDls , 'distribution': distribution}
-        if(downloadsMetric === 'Nombre applications' ) { 
-            preprocessedValue['Nombre applications'] = nAppByValue.has(value) ? nAppByValue.get(value) : 0
-        }
-        if (downloadsMetric === 'Nombre applications moyen'){
-            preprocessedValue['Nombre applications moyen'] = (nAppByValue.has(value) ? nAppByValue.get(value) : 0) / occurrencesByValue.get(value)
-        }
+        let nApp = nAppByValue.has(value) ? nAppByValue.get(value) : 0
+        let meanNApp = (nAppByValue.has(value) ? nAppByValue.get(value) : 0) / occurrencesByValue.get(value)
+
+        let preprocessedValue = { 'value': value, 'sum': {'value' : sumDls}, 'avg': {'value' : meanDls} , 'distribution': {'value':distribution}, 'nApp' : {'value':nApp},
+         'avgNApp' :{'value': meanNApp}}
         preprocessedData.push(preprocessedValue)
     }
-
+    addPositionsMetrics(preprocessedData)
     handleSort(preprocessedData, isAscending, downloadsMetric)
     return preprocessedData
 }
 
-export const CONSTANTS = {
-    variableSelector : {
-        values : ['Category','Genres', 'Type', 'Content Rating', 'Android Ver'],
-        texts : ['Categorie','Genres', 'Type', 'Evaluation de contenu', 'Version minimale d’Android requise'],
-        helper: 'Choisir la variable categorique a visualiser',
-        label: 'Variable'
-    },
-    downloadsMetricSelector : {
-        values : ['brut','moyen', 'Nombre applications', 'Nombre applications moyen'],
-        texts : ['Somme','Moyenne', 'Nombre applications avec +', 'Nombre applications avec + moyen'],
-        helper: 'Choisir la metrique de telechargement a visualiser',
-        label: 'Metrique de telechargements'
-    },
-    radioButtons: {
-        values : [true, false],
-        texts : ['Croissant','Decroissant'],
-        label:'Ordonnancement'
-    },
-    title : 'Visualisation 1 : Exploration du comportement des variables catégoriques et des téléchargements',
-    modal: {
-        title : 'Informations supplementaires',
-        content : 'TODO',
-    },
-  
-}
+
--- src/viz1/viz1.jsx
+++ src/viz1/viz1.jsx
@@ -10,7 +10,8 @@ import { preprocessData, getDownloadsRanges, CONSTANTS } from './utils';
 import { Selector } from '../components/Selector';
 import { RadioButtons } from '../components/RadioButtons';
 import { Modal } from '../components/Modal';
-
+import { DataTable } from '../components/Table';
+import '../index.css';
 
 const createSVG = () => {
     return d3.select('.svg')
@@ -23,12 +24,11 @@ const createSVG = () => {
 
 export function Categorical() {
     const [isAscending, setAscending] = React.useState(true)
-    const [downloadsMetric, setDownloadMetric] = React.useState('brut')
+    const [downloadsMetric, setDownloadMetric] = React.useState(CONSTANTS.downloadsMetricSelector.values[0])
     const [variable, setVariable] = React.useState('Category')
     const [downloadsRange, setDownloadsRange] = React.useState('1,000,000,000+')
     const [downloadsRanges, setDownloadsRanges] = React.useState(null)
     const [isModalOpen, setModalOpen] = React.useState(false)
-    const [isToolTipOpen, setToolTip] = React.useState(false)
 
     const createVisusalisation = () => {
         d3.csv(CSV_URL).then((data, error) => {
@@ -39,20 +39,20 @@ export function Categorical() {
 
             let preprocessedData = preprocessData(data, downloadsMetric, variable, isAscending, downloadsRange)
             let svg = createSVG()
-
+            
             let xScale = d3.scaleLinear()
-                .domain([d3.min(preprocessedData.map(row => row[downloadsMetric])), d3.max(preprocessedData.map(row => row[downloadsMetric]))])
+                .domain([d3.min(preprocessedData.map(row => row[downloadsMetric].value)), d3.max(preprocessedData.map(row => row[downloadsMetric].value))])
                 .range([0, window.screen.width * 0.75])
 
             svg.append("g")
                 .attr("transform", "translate(-10,15)")
+                .style("position", "fixed") //todo : fix la position
                 .call(d3.axisBottom(xScale))
                 .append("text")
-
                 .style("text-anchor", "end")
                 .attr("font-family", "sans-serif")
                 .attr("font-size", "14px")
-        
+
             let yScale = d3.scaleBand()
                 .range([0, preprocessedData.length * 50])
                 .domain(preprocessedData.map(row => row.value))
@@ -76,29 +76,79 @@ export function Categorical() {
                 .attr('x', '80%')
                 .text("Metrique de telechargement : " + downloadsMetric);
 
+            let toolTip = d3.select(".svg").append("div")
+                .attr("class", "tooltip")
+                .style("opacity", 0);
+            // bars container
             let bars = svg.selectAll("myRect")
                 .data(preprocessedData)
                 .enter()
                 .append('g')
                 .attr("transform", "translate(-10," + 20 + ")")
 
+
+            //real bars
             bars.append("rect")
                 .attr("y", (row) => yScale(row.value))
-                .attr("width", (row) => xScale(row[downloadsMetric]))
+                .attr("width", (row) => xScale(row[downloadsMetric].value))
                 .attr("height", () => yScale.bandwidth())
                 .attr("fill", "steelblue")
+                .attr('opacity', 0.7)
+                .attr('id', (row, i) => 'bar-' + i)
+                .on('mouseover', function (event, row) {
+                    d3.select(this)
+                        .transition()
+                        .duration(50)
+                        .attr('opacity', 1)
+
+                    toolTip.transition()
+                        .duration(50)
+                        .style('opacity', 1)
+
+                    toolTip.html(row.distribution.value)
+                        .style("left", (event.pageX + 20) + "px")
+                        .style("top", (event.pageY - 20) + "px")
+                })
+                .on('mouseout', function (event, row) {
+                    d3.select(this)
+                        .transition()
+                        .duration(50)
+                        .attr('opacity', 0.7)
+
+                    toolTip.transition()
+                        .duration(50)
+                        .style('opacity', 0)
+                })
 
             bars.append('text')
-                .text(row => row[downloadsMetric])
+                .text(row => row[downloadsMetric].value)
                 .style("text-anchor", "middle")
-                .attr("x", (row) => xScale(row[downloadsMetric]) * 0.8)
+                .attr("x", (row) => xScale(row[downloadsMetric].value) * 0.8)
                 .attr("y", (row) => yScale(row.value) + yScale.bandwidth() - 10)
                 .attr("font-family", "sans-serif")
                 .attr("font-size", "14px")
                 .attr("fill", "black")
+                .on('mouseover', function (event, row) {
+                    d3.select(this)
+                    toolTip.transition()
+                        .duration(50)
+                        .style('opacity', 1)
 
-            bars.on('click', () => setToolTip(true))
-            bars.style('cursor', 'pointer')
+                    toolTip.html(row.distribution.value)
+                        .style("left", (event.pageX + 20) + "px")
+                        .style("top", (event.pageY - 20) + "px")
+                })
+                .on('mouseout', function (event, row) {
+                    d3.select(this)
+                        .transition()
+                        .duration(50)
+                        .attr('opacity', 0.7)
+
+                    toolTip.transition()
+                        .duration(50)
+                        .style('opacity', 0)
+                })
+                
 
             setDownloadsRanges(getDownloadsRanges(data))
 
@@ -110,18 +160,18 @@ export function Categorical() {
         createVisusalisation()
     }, [downloadsMetric, variable, isAscending, downloadsRange])
 
-    const shouldDisplayDlsRangesSelector = (downloadsMetric === 'Nombre applications' || downloadsMetric === 'Nombre applications moyen')
+    const shouldDisplayDlsRangesSelector = (downloadsMetric === CONSTANTS.downloadsMetricSelector.values[3] || downloadsMetric === CONSTANTS.downloadsMetricSelector.values[4])
 
     const Selectors = () => (<Box sx={{ display: 'flex', justifyContent: 'space-between', maxWidth: '60%', paddingLeft: '5%' }}>
-        <Selector inputLabel={CONSTANTS.variableSelector.label} 
+        <Selector inputLabel={CONSTANTS.variableSelector.label}
             currentValue={variable} onChange={(event) => setVariable(event.target.value)} menuItemsValues={CONSTANTS.variableSelector.values} menuItemsText={CONSTANTS.variableSelector.texts} helperText={CONSTANTS.variableSelector.helper}
         />
-        <Selector inputLabel={CONSTANTS.downloadsMetricSelector.label} 
+        <Selector inputLabel={CONSTANTS.downloadsMetricSelector.label}
             currentValue={downloadsMetric} onChange={(event) => setDownloadMetric(event.target.value)} menuItemsValues={CONSTANTS.downloadsMetricSelector.values} menuItemsText={CONSTANTS.downloadsMetricSelector.texts} helperText={CONSTANTS.downloadsMetricSelector.helper}
         />
         {shouldDisplayDlsRangesSelector &&
-            <Selector inputLabel={'Nombre de telechargements'} 
-            currentValue={downloadsRange} onChange={(event) => setDownloadsRange(event.target.value)} menuItemsValues={downloadsRanges ? downloadsRanges : []} menuItemsText={downloadsRanges ? downloadsRanges : []} helperText={"Choisir la tranche de telechargement"}
+            <Selector inputLabel={'Nombre de telechargements'}
+                currentValue={downloadsRange} onChange={(event) => setDownloadsRange(event.target.value)} menuItemsValues={downloadsRanges ? downloadsRanges : []} menuItemsText={downloadsRanges ? downloadsRanges : []} helperText={"Choisir la tranche de telechargement"}
             ></Selector>
         }
     </Box>
@@ -134,9 +184,9 @@ export function Categorical() {
                 {CONSTANTS.title}
             </Typography>
             <Box pl={'40%'} pt={3}>
-                <RadioButtons label={CONSTANTS.radioButtons.label} currentValue={isAscending} onChange = {(event) => setAscending(event.target.value === "true")}
-                 buttonsValues ={CONSTANTS.radioButtons.values} buttonsText={CONSTANTS.radioButtons.texts}></RadioButtons>
-                <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title={CONSTANTS.modal.title} content={CONSTANTS.modal.content}/>
+                <RadioButtons label={CONSTANTS.radioButtons.label} currentValue={isAscending} onChange={(event) => setAscending(event.target.value === "true")}
+                    buttonsValues={CONSTANTS.radioButtons.values} buttonsText={CONSTANTS.radioButtons.texts}></RadioButtons>
+                <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title={CONSTANTS.modal.title} content={CONSTANTS.modal.content} />
                 <HelpIcon onClick={() => setModalOpen(true)} />
             </Box>
             <Box pl={'25%'} pt={3} pb={2}>
