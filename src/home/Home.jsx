d09be41f83916b456cfa220afca568921e1f82f1
Felix Tremblay <tremblay.felix@hotmail.com>
2022-06-24 20:05:27 -0400

    added basic pics for homepage

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
       <CardMedia
         component="img"
         height="200"
-        image={require("../téléchargement.jpeg")}
+        image={require("./viz1.png")}
         alt="green iguana"
       />
       <CardContent>
@@ -65,7 +65,84 @@ function Home() {
         </Typography>
 
       <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }} pt={5}>
-        {cards}
+        <Card sx={{ width: 500, border: 1, height: 'auto'}} elevation={12}>
+          <CardMedia
+            component="img"
+            height="200"
+            image={require("./viz1.png")}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              Visualisation titre
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              La première visualisation est un outil qui permet d’explorer le comportement des variables catégoriques en fonction du nombre de téléchargements.
+              Ainsi, la visualisation permettra de comparer chacune des variables (une à la fois) suivantes avec une des métriques représentant le nombre de téléchargements (une à la fois)  :
+              Catégorie d’application
+              Évaluation de contenu
+              Version minimale d’Android requise
+              Genres
+              Type (application payante ou application gratuite)
+
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="medium" component={Link} to={'/categorical'}>Voir la visualisation</Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ width: 500, border: 1, height: 'auto'}} elevation={12}>
+          <CardMedia
+            component="img"
+            height="200"
+            image={require("./viz2.png")}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              Visualisation titre
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              La première visualisation est un outil qui permet d’explorer le comportement des variables catégoriques en fonction du nombre de téléchargements.
+              Ainsi, la visualisation permettra de comparer chacune des variables (une à la fois) suivantes avec une des métriques représentant le nombre de téléchargements (une à la fois)  :
+              Catégorie d’application
+              Évaluation de contenu
+              Version minimale d’Android requise
+              Genres
+              Type (application payante ou application gratuite)
+
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="medium" component={Link} to={'/categorical'}>Voir la visualisation</Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ width: 500, border: 1, height: 'auto'}} elevation={12}>
+          <CardMedia
+            component="img"
+            height="200"
+            image={require("./viz3.png")}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              Visualisation titre
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              La première visualisation est un outil qui permet d’explorer le comportement des variables catégoriques en fonction du nombre de téléchargements.
+              Ainsi, la visualisation permettra de comparer chacune des variables (une à la fois) suivantes avec une des métriques représentant le nombre de téléchargements (une à la fois)  :
+              Catégorie d’application
+              Évaluation de contenu
+              Version minimale d’Android requise
+              Genres
+              Type (application payante ou application gratuite)
+
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="medium" component={Link} to={'/categorical'}>Voir la visualisation</Button>
+          </CardActions>
+        </Card>
       </Box>
     </Box>
   );
